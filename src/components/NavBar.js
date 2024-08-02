b5bfc1405a828298445603fa00c65318c9a32f73
Morgan Asare <ohenemorgan@gmail.com>
2024-08-02 19:34:42 +0000

    add navlink to navigate pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function NavBar() {
   return (
-    <Navbar expand="md" fixed='top'>
+    <Navbar className={styles.NavBar} expand="md" fixed='top'>
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt='logo' height='45'/>
-        </Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>
+            <img src={logo} alt='logo' height='45'/>
+          </Navbar.Brand>
+          </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-start">
-            <Nav.Link><i class="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+            <NavLink to="/" className={styles.NavLink}
+              activeClassName={styles.Active}><i class="fa-solid fa-house"></i>Home</NavLink>
+
+            <NavLink to="/signin" className={styles.NavLink}
+              activeClassName={styles.Active}><i class="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
+
+            <NavLink to="/signup" className={styles.NavLink}
+              activeClassName={styles.Active}><i class="fa-solid fa-user-plus"></i>Sign up</NavLink>
            
           </Nav>
         </Navbar.Collapse>
