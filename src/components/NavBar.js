aabe0591c58ebcfca96bb8b0350a303894989519
Morgan Asare <ohenemorgan@gmail.com>
2024-08-04 20:33:24 +0000

    fix routing to all pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,29 +4,30 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css";
-import { NavLink } from 'react-bootstrap';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
       <Container>
-        <Link to="/">
+        <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt='logo' height='45'/>
           </Navbar.Brand>
-          </Link>
+          </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-start">
-            <NavLink to="/" className={styles.NavLink}
-              activeClassName={styles.Active}><i class="fa-solid fa-house"></i>Home</NavLink>
+            <NavLink 
+            exact
+            to="/" 
+            className={styles.NavLink} activeClassName={styles.Active}><i class="fa-solid fa-house"></i>Home</NavLink>
 
-            <NavLink to="/signin" className={styles.NavLink}
-              activeClassName={styles.Active}><i class="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
+            <NavLink to="/signin" 
+              className={styles.NavLink} activeClassName={styles.Active}><i class="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
 
-            <NavLink to="/signup" className={styles.NavLink}
-              activeClassName={styles.Active}><i class="fa-solid fa-user-plus"></i>Sign up</NavLink>
+            <NavLink to="/signup" 
+            className={styles.NavLink} activeClassName={styles.Active}><i class="fa-solid fa-user-plus"></i>Sign up</NavLink>
            
           </Nav>
         </Navbar.Collapse>
